42ccb1216ebaeee0e4c4e752425345b288a848e1
ministrov <61511030+ministrov@users.noreply.github.com>
2020-07-07 19:58:13 +0300

    finalized popup js

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,14 @@
 var buttonContacts = document.querySelector(".button-contacts");
 var popup = document.querySelector(".modal");
 var popupClose = document.querySelector(".close-form");
-
-buttonContacts.addEventListener("click", function () {
+var loginField = document.querySelector(".login-input")
+buttonContacts.addEventListener("click", function (evt) {
+  evt.preventDefault();
   popup.classList.add("modal-show");
-})
+  loginField.focus();
+});
 
-popupClose.addEventListener("click", function () {
+popupClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
   popup.classList.remove("modal-show");
-})
+});
